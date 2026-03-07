64c22349198f2863efb13d660918064ce3188847
Leonid Azgihin <leonidazgihin@MacBook-Air-Leonid.local>
2026-03-08 02:38:36 +0400

    Libraries

--- include/shed.h
+++ include/shed.h
@@ -24,28 +24,14 @@ typedef struct task {
     size_t size;
 } task_t;
 
-// глобальные задачи
 extern task_t tasks[MAX_TASKS];
 extern int current_task;
 
-// инициализация
 void shed_init();
-
-// добавить функцию
 int shed_add(void (*fn)(), const char* name);
-
-// добавить бинарник
-int shed_add_binary(const char* name, uint8_t* buf, size_t size);
-
-// убить задачу
 void shed_kill(int pid);
-
-// переключение
 void shed_yield();
 void shed_tick();
-
-// функция для запуска бинарника
-void run_program(uint8_t* buf, size_t size);
 
 const char* state_to_string(task_state_t state);
 
