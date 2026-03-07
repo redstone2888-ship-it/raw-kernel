9d85dec4ce74b30f280ed747da52e5bb0d05dc4e
Leonid Azgihin <leonidazgihin@MacBook-Air-Leonid.local>
2026-03-08 02:37:23 +0400

    Boot

--- boot/boot.s
+++ boot/boot.s
@@ -1,4 +1,12 @@
-; boot.s (NASM syntax)
+; boot.s - GRUB Bootloader entry point
+
+; Copyright (c) 2026, Redstone2888
+; Read LICENSE.txt for details
+
+; This file sets up the environment for the kernel and is required
+; for proper booting via GRUB. 
+; DO NOT DELETE THIS FILE — removing it will prevent the kernel from booting.
+
 section .multiboot
 align 4
     dd 0x1BADB002                 ; magic
@@ -9,12 +17,14 @@ section .text
 extern kernel_main
 global _start
 
+; We push eax, ebx onto the stack and pass them to the kernel
 _start:
     push ebx
     push eax
     
     call kernel_main
 
+; If kernel_main crashed (inpossible), hanging out
 .loop:
     hlt
     jmp .loop
